3e6466a8586bb21bc2ca86e2e1a3aa71a7edbdf0
黄志磊 <huangzl@shinemo.com>
2016-03-16 11:12:41 +0800

    bug fix

--- mpush-common/src/main/java/com/shinemo/mpush/common/conn/module/ConnServerModule.java
+++ mpush-common/src/main/java/com/shinemo/mpush/common/conn/module/ConnServerModule.java
@@ -3,11 +3,16 @@ package com.shinemo.mpush.common.conn.module;
 import com.shinemo.mpush.api.container.BaseLifeCycle;
 import com.shinemo.mpush.api.spi.ServiceContainer;
 import com.shinemo.mpush.common.conn.ConnectionServerManage;
+import com.shinemo.mpush.common.conn.plugin.ConnServerPlugin;
 
 
 public class ConnServerModule extends BaseLifeCycle{
 	
 	private ConnectionServerManage connectionServerManage = ServiceContainer.getInstance(ConnectionServerManage.class, "connectionServerManage");
+	
+	public ConnServerModule() {
+		addLifeCycleListener(new ConnServerPlugin());
+	}
 	
 	@Override
 	public void start0() {
