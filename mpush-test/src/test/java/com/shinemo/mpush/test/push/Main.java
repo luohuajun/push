e1959a57e86775b28cd1b90a9a157ed8c2477c76
黄志磊 <huangzl@shinemo.com>
2016-03-24 17:10:32 +0800

    bug fix

--- mpush-test/src/test/java/com/shinemo/mpush/test/push/Main.java
+++ mpush-test/src/test/java/com/shinemo/mpush/test/push/Main.java
@@ -1,48 +1,40 @@
 package com.shinemo.mpush.test.push;
 
-import com.shinemo.mpush.api.PushContent;
-import com.shinemo.mpush.api.PushSender;
-import com.shinemo.mpush.api.PushContent.PushType;
-import com.shinemo.mpush.push.PushClient;
-import com.shinemo.mpush.tools.Jsons;
-
-import java.util.Arrays;
+import com.shinemo.mpush.api.Future;
+import com.shinemo.mpush.client.MpushClient;
+import com.shinemo.mpush.client.Request;
 import java.util.concurrent.locks.LockSupport;
 
-/**
- * Created by ohun on 2016/1/7.
- */
 public class Main {
     public static void main(String[] args) throws Exception {
-        PushClient client = new PushClient();
-//        client.start();
-        Thread.sleep(1000);
+    	
+    	MpushClient client = new MpushClient("", "", "");
+    	
         for (int i = 0; i < 100; i++) {
-        	PushContent content = PushContent.build(PushType.MESSAGE,"this a first push." + i);
-        	content.setMsgId("msgId_" + (i % 2));
-
-            client.send(Jsons.toJson(content), Arrays.asList("huang1", "huang2", "huang"), new PushSender.Callback() {
-                @Override
-                public void onSuccess(String userId) {
-                    System.err.println("push onSuccess userId=" + userId);
-                }
-
-                @Override
-                public void onFailure(String userId) {
-                    System.err.println("push onFailure userId=" + userId);
-                }
-
-                @Override
-                public void onOffline(String userId) {
-                    System.err.println("push onOffline userId=" + userId);
-                }
-
-                @Override
-                public void onTimeout(String userId) {
-                    System.err.println("push onTimeout userId=" + userId);
-                }
-            });
-            Thread.sleep(10000);
+        	
+        	client.newRequest(Request.build("huang1", "hello world", new Future.Callback() {
+				
+				@Override
+				public void onTimeout(String userId) {
+					 System.err.println("push onTimeout userId=" + userId);
+				}
+				
+				@Override
+				public void onSuccess(String userId) {
+					System.err.println("push onSuccess userId=" + userId);
+				}
+				
+				@Override
+				public void onOffline(String userId) {
+					System.err.println("push onOffline userId=" + userId);
+				}
+				
+				@Override
+				public void onFailure(String userId) {
+					 System.err.println("push onFailure userId=" + userId);
+				}
+			}));
+        	
         }
         LockSupport.park();
     }
